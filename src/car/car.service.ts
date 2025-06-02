921df075f712ceda7dc373a671c42bf8d3546596
Alexandr A <sandriq@MacBook-Pro-Alexandr.local>
2025-06-02 21:36:29 +0500

    minor fix

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,24 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Car } from './entities/car.entity';
 import { Repository } from 'typeorm';
+import { Model } from './model/entities/model.entity';
 
 @Injectable()
 export class CarService {
   constructor(
     @InjectRepository(Car)
-    private repo: Repository<Car>
+    private repo: Repository<Car>,
+    @InjectRepository(Model)
+    private modelRepo: Repository<Model>
   ) { }
   async create(dto: CreateCarDto): Promise<Car> {
+    const { modelId, brandId } = dto;
+    const model = await this.modelRepo.findOne({
+      where: { id: modelId },
+      relations: ['brand'],
+    });
+    if (!model || model.id !== modelId || model.brand.id !== brandId) {
+      throw new BadRequestException(`Wrong modelId ${modelId} or brandId ${brandId}`);
+    }
     const entity = new Car(dto)
+    entity.brand = model.brand;
+    entity.model = model;
     return this.repo.save(entity)
   }
 
   async findAll(): Promise<Car[]> {
     return this.repo.find(
-      {relations: ['brand', 'model'],}
+      { relations: ['brand', 'model'], }
     )
   }
 
