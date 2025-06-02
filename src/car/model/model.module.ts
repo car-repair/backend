755e2613008bc2e2e63587d4a9c9ce698fc30094
Alexandr A <sandriq@MacBook-Pro-Alexandr.local>
2025-06-02 18:55:40 +0500

    minor fixes

--- src/car/model/model.module.ts
+++ src/car/model/model.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Model } from './entities/model.entity';
 import { ModelService } from './model.service';
-import { ModelController } from './model.controller';
+//import { ModelController } from './model.controller';
 import { Brand } from '../brand/entities/brand.entity'; // Обновляем путь
 
 @Module({
   imports: [TypeOrmModule.forFeature([Model, Brand])],
   providers: [ModelService],
-  controllers: [ModelController],
+  //controllers: [ModelController],
   exports: [ModelService],
 })
 export class ModelModule {}
